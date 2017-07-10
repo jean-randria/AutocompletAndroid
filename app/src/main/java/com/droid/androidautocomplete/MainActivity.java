4c5553d3209f178d8c9d2efd3d204bfe85698495
jean-randria <benja72@gmail.com>
2017-07-10 18:24:46 +0200

    commit initial Utilisation de l'autocomplete avec android

---
+++ app/src/main/java/com/droid/androidautocomplete/MainActivity.java
@@ -0,0 +1,27 @@
+package com.droid.androidautocomplete;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String[] language ={"C","C++","Java",".NET","iPhone","Android","ASP.NET","PHP"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Creating the instance of ArrayAdapter containing list of language names
+        ArrayAdapter<String> adapter = new ArrayAdapter<String> (this,android.R.layout.select_dialog_item,language);
+
+        AutoCompleteTextView actv= (AutoCompleteTextView)findViewById(R.id.autoCompleteTextView1);
+        actv.setThreshold(1);//will start working from first character
+        actv.setAdapter(adapter);//setting the adapter data into the AutoCompleteTextView
+        actv.setTextColor(Color.RED);
+    }
+}
+
